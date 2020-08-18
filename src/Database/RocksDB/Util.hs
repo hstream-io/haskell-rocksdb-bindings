7c400f7d6d1d4bce347e6f7f595d210602d75128
Wangbin <wangbin@emqx.io>
2020-08-18 10:33:15 +0800

    Fix ffi type CULLong error on linux

--- src/Database/RocksDB/Util.hs
+++ src/Database/RocksDB/Util.hs
@@ -4,7 +4,7 @@ import Data.Binary (Binary, decode, encode)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as BSL
 import Data.Word (Word32, Word64)
-import Foreign.C (CULLong)
+import Foreign.C (CULLong, CULong)
 import Foreign.C.Types (CInt, CSize, CUInt)
 
 intToCInt :: Int -> CInt
@@ -27,9 +27,6 @@ word64ToCSize = fromIntegral
 
 word32ToCUInt :: Word32 -> CUInt
 word32ToCUInt = fromIntegral
-
-cULLongToWord64 :: CULLong -> Word64
-cULLongToWord64 = fromIntegral
 
 serialize :: Binary b => b -> ByteString
 serialize = BSL.toStrict . encode
